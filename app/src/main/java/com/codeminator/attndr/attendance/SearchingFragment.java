165a25b7d6c2e98fc3bbc28c3a0ddbc303602e74
roalts <raghav.apoorv@gmail.com>
2016-03-19 17:56:45 +0530

    range fixed

--- app/src/main/java/com/codeminator/attndr/attendance/SearchingFragment.java
+++ app/src/main/java/com/codeminator/attndr/attendance/SearchingFragment.java
@@ -167,7 +167,6 @@ public class SearchingFragment extends Fragment {
                         apneBeacon.clear();
 
                         for (int b = 0; b < beacons.size(); b++) {
-                            apneBeacon.add(beacons.get(b));
                             if(com.estimote.sdk.Utils.computeAccuracy(beacons.get(b)) <= 1.00) {
                                 apneBeacon.add(beacons.get(b));
                             }
